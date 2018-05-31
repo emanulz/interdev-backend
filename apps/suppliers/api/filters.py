abe78e18b66b4aea246c8a3783a7d909f4849489
VictorMora <victor.mora.benambourg@gmail.com>
2018-05-31 07:38:28 -0600

    Disabled  post save product channels signal

--- apps/suppliers/api/filters.py
+++ apps/suppliers/api/filters.py
@@ -4,10 +4,17 @@ from __future__ import unicode_literals
 import django_filters
 from ..models import Supplier
 
+import django_filters
 
 class SupplierFilter(django_filters.FilterSet):
-
+    
+    phone_number = django_filters.CharFilter(lookup_expr='icontains')
+    created = django_filters.DateFilter(name='created', lookup_expr='year')
+    created__gt = django_filters.DateFilter(name='created_gt', lookup_expr='year__gt')
+    
     class Meta:
         model = Supplier
         fields = ('id', 'code', 'name', 'id_type', 'id_num', 'address', 'phone_number', 'cellphone_number', 'email',
                   'agent_name', 'agent_last_name', 'agent_phone_number', 'agent_email', 'created', 'updated')
+
+
