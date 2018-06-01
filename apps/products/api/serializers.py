82183443938b0c546e585bb880841a58b15c2100
VictorMora <victor.mora.benambourg@gmail.com>
2018-06-01 13:37:20 -0600

    Serializers inv sum optimization

--- apps/products/api/serializers.py
+++ apps/products/api/serializers.py
@@ -2,10 +2,12 @@
 from __future__ import unicode_literals
 import json
 import serpy
-
+from django.db.models import Sum
 from rest_framework import serializers
 from apps.inventories.models import Inventory_Movement, Warehouse
 from ..models import Product, ProductDepartment, ProductSubDepartment
+
+import time
 
 
 class ProductFasterSerializer(serpy.Serializer):
@@ -71,8 +73,11 @@ class ProductFasterSerializer(serpy.Serializer):
 
 class ProductSerializer(serializers.ModelSerializer):
 
-    inventory = serializers.SerializerMethodField()
+    #inventory = serializers.SerializerMethodField()
     inventory_by_warehouse = serializers.SerializerMethodField()
+    counter = 0
+    total_inv = 0
+    inv_by_warehouse = {}
 
     class Meta:
         model = Product
@@ -82,28 +87,77 @@ class ProductSerializer(serializers.ModelSerializer):
                   'utility', 'utility2', 'utility3', 'price', 'price2', 'price3', 'ask_price', 'use_taxes', 'taxes',
                   'use_taxes2', 'taxes2', 'pred_discount', 'is_active', 'consignment', 'generic', 'image',
                   'observations', 'created', 'updated', 'max_sale_discount', 'on_sale', 'cost_based',
-                  'sell_price', 'sell_price2', 'sell_price3', 'inventory', 'inventory_by_warehouse')
+                  'sell_price', 'sell_price2', 'sell_price3', 'inventory_by_warehouse')
 
     def get_inventory(self, obj):
-        return getProductInventory(obj.id)
+        t1 = time.time()
+        a =  getProductInventory(obj.id)
+        #print('How many trips? ' + str(ProductSerializer.counter))
+        t2 = time.time()
+        print("Run time: " + str((t2 - t1)) + "\n")
+        return a
 
     def get_inventory_by_warehouse(self, obj):
-        return inventoryAllWarehouses(obj.id)
+        #t1 = time.time()
+        #a = inventoryAllWarehouses(obj.id)
+        #t2 = time.time()
+        #print("Run time: " + str((t2 - t1)) + "\n")
+        #return a
+        return getProductInventory2(obj.id)
+
+def getProductInventory3(product_id):
+    t1 = time.time()
+    warehouses = Warehouse.objects.all()
+    inv_dict = {}
+
+    total_inv = 0
+    by_prod = Inventory_Movement.objects.filter(product_id=product_id)
+    for warehouse in warehouses:
+        search2 = str(warehouse.id).replace('-', '')
+        tot = by_prod.filter(warehouse_id = search2).aggregate(Sum('amount'))['amount__sum']
+        if tot!=None:
+            inv_dict[str(warehouse.id)] = float(tot)
+            total_inv+= tot
+        else:
+            inv_dict[str(warehouse.id)] = 0
+            total_inv += 0
+
+    t2 = time.time()
+    print("Run time: " + str((t2 - t1)) + "\n")
+    return json.dumps(inv_dict)
+
+
+def getProductInventory2(product_id):
+    t1 = time.time()
+    warehouses = Warehouse.objects.all()
+    inv_dict = {}
+    total_inv = 0
+    for warehouse in warehouses:
+        search2 = str(warehouse.id).replace('-', '')
+        a = Inventory_Movement.objects.all()
+        a.count()
+
+    t2 = time.time()
+
+    print("Run time: " + str((t2 - t1)) + "\n")
+    return ''
+
+
+
+
 
 
 def getProductInventory(product_id):
-
+    ProductSerializer.counter += 1
     warehouses = Warehouse.objects.all()
     amount = 0
     for warehouse in warehouses:
         inventory = inventoryByWarehouse(product_id, warehouse.id)
         amount = amount + inventory
-
     return amount
 
 
 def inventoryAllWarehouses(product_id):
-
     warehouses = Warehouse.objects.all()
     inventoriesDict = {}
     for warehouse in warehouses:
