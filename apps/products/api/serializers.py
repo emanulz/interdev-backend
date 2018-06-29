fc110b9288b2cddb48537497fe2ce857209022c9
VictorMora <victor.mora.benambourg@gmail.com>
2018-06-29 10:16:20 -0600

    Changed serializer of product to use __all__

--- apps/products/api/serializers.py
+++ apps/products/api/serializers.py
@@ -20,13 +20,7 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ('id', 'code', 'description', 'short_description', 'unit', 'fractioned', 'department', 'subdepartment',
-                  'barcode', 'internal_barcode', 'supplier_code', 'model', 'part_number', 'brand_code',
-                  'inventory_enabled', 'inventory_minimum', 'inventory_maximum', 'inventory_negative', 'cost',
-                  'utility', 'price', 'ask_price', 'use_taxes', 'taxes',
-                  'use_taxes2', 'taxes2', 'pred_discount', 'is_active', 'consignment', 'generic', 'image',
-                  'observations', 'created', 'updated', 'max_sale_discount', 'on_sale', 'cost_based',
-                  'sell_price', 'inventory_existent', 'max_regular_discount')
+        fields = '__all__'
 
 
 class ProductDepartmentSerializer(serializers.ModelSerializer):
