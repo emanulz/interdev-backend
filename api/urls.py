9a1c1eabb6679d70c4bfdbb80cb6c4287511f4a7
VictorMora <victor.mora.benambourg@gmail.com>
2018-10-18 09:09:47 -0600

    Added view to handle Additions object

--- api/urls.py
+++ api/urls.py
@@ -28,7 +28,7 @@ from purchases.api.views import PurchaseViewSet, PurchaseCreateViewSet, Purchase
 from dynamic_preferences.users.viewsets import UserPreferencesViewSet
 from payables.api.views import Credit_MovementPayableViewSet, Credit_PaymentPayableViewSet, CreditPaymentCreateViewSetPayables
 from presales.api.views import PresaleViewSet, PresalePatchViewSet
-from sales.api.views import SaleCreateViewSet, SaleViewSetReadOnly, ReturnViewSet, CurrencyViewset
+from sales.api.views import SaleCreateViewSet, SaleViewSetReadOnly, ReturnViewSet, CurrencyViewset, AdditionViewSetReadOnly
 from money_returns.api.views import Money_ReturnViewSet, Credit_VoucherViewSet
 from payables_money_returns.api.views import Credit_VoucherViewSetPayable
 from utils.searchView import SearchViewSet
@@ -56,6 +56,7 @@ router.register(r'productslist', ProductViewSet)
 router.register(r'productsrestaurant', RestaurantProdsViewset)
 router.register(r'currency', CurrencyViewset)
 router.register(r'sales', SaleCreateViewSet, base_name='sales')
+router.register(r'additions', AdditionViewSetReadOnly)
 router.register(r'saleslist', SaleViewSetReadOnly)
 router.register(r'clients', ClientViewSet)
 router.register(r'clientcategories', ClientCategoryViewSet)
